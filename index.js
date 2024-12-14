32ba2b5c81676ebc6b6d9402ef65ce9af86f987c
IftekarRahmanRuhit <iftekarrahmanruhit2003@gmail.com>
2024-12-14 20:22:50 +0600

    added functionality for deleting a job from db

--- index.js
+++ index.js
@@ -51,6 +51,14 @@ async function run() {
       res.send(result);
     });
 
+    // deleting a job from db
+    app.delete("/job/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await jobsCollection.deleteOne(query);
+      res.send(result);
+    });
+
     await client.db("admin").command({ ping: 1 });
     console.log(
       "Pinged your deployment. You successfully connected to MongoDB!"
